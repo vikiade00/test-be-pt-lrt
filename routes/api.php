9e573a1ee64a2cd5931345248309caccfbdeed02
vikiade00 <vikiade00@gmail.com>
2024-11-27 12:28:54 +0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::apiResource('/barang', App\Http\Controllers\Api\BarangController::class);
